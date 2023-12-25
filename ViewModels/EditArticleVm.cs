5a881da63c516d0621dd9b0c2ce78e96cc932764
Hossein Molaei <hosein.molaei3@gmail.com>
2023-12-26 01:02:29 +0330

    article file

---
+++ ViewModels/EditArticleVm.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.ViewModels;
+
+public class EditArticleVm
+{
+    
+    public int ArticleId { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    
+    public IFormFile? Image { get; set; }
+    public string? ImageName { get; set; }
+    
+    [Required]
+    public DateTime CreateTime { get; set; }
+    public string? TimeRead { get; set; }
+    [Required]
+    public int UserId { get; set; }
+}
