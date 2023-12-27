90d930141a8306c8f63e37470b7810fe80d002d6
Hossein Molaei <hosein.molaei3@gmail.com>
2023-12-27 23:55:27 +0330

    other

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Test.Context;
+using Test.ViewModels.ProductVM;
+
+namespace Test.Areas.Admin.Controllers;
+
+public class ProductController : AdminBaseController
+{
+    #region context
+    private readonly ApplicationContext _db;
+    public ProductController(ApplicationContext context)
+    {
+        _db = context;
+    }
+    #endregion
+   
+    // GET
+    public IActionResult ShowProduct()
+    {
+        ViewData["Title"] = "Product";
+        return View(_db.ProductModels.ToList());
+    }
+
+    #region Create Product
+
+    [HttpGet]
+    public IActionResult CreateProduct()
+    {
+        return View();
+    }
+
+    [HttpPost,ValidateAntiForgeryToken]
+    public IActionResult CreateProduct(CreateProductVM createProductVm)
+    {
+        var userid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (ModelState.IsValid)
+        {
+           
+        }
+        return RedirectToAction("ShowProduct", "Product", new { area = "Admin" });
+    }
+    #endregion
+}
---
+++ ViewModels/ProductVM/CreateProductVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.ViewModels.ProductVM;
+
+public class CreateProductVM
+{
+    [Required]
+    [Display(Name = "Name")]
+    public string ProductName { get; set; }
+    [Required]
+    [Display(Name = "Description")]
+    public string ProductDescription { get; set; }
+    [Required]
+    [Display(Name = "Price")]
+    public decimal Price { get; set; }
+    [Required]
+    [Display(Name = "User Id")]
+    public int UserId { get; set; }
+}
